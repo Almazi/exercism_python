7b56504a6905bf4e58734cae3ffbf8a5ee2074ae
almazi <navilalmazi@gmail.com>
2017-02-22 19:56:17 +0600

    First two codes

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+#
+# Skeleton file for the Python "Hello World" exercise.
+#
+
+
+def hello(name='World'):
+    if name:
+        return "Hello, %s!" %name
+    else:
+        return "Hello, World!"
